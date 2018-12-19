93b2d80f0564084ad646eb567f7e82eca0e95a48
potatoFr1es <torbenborstl@gmail.com>
2018-12-19 22:39:58 +0300

    nothing new2

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -43,12 +43,12 @@ class PostController extends AbstractController
 
     public function postsAction(){
 
-        $postsList = "number of list";
+        $text = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. ";
 
 
         return $this->render( 'post/posts.html.twig' ,
             [
-                'posts' => $postsList,
+            'post' => $text,
             ]);
 
     }
